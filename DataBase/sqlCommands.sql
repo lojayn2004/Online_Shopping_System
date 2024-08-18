31a7965c9b22a940df985720a47da1494a007b20
lojayn2004 <111882914+lojayn2004@users.noreply.github.com>
2024-08-18 22:44:36 +0300

    Create sqlCommands.sql

---
+++ DataBase/sqlCommands.sql
@@ -0,0 +1,28 @@
+
+
+create table Products(
+    productId int primary key identity(1, 1),
+	productName varchar(70),
+	productDescription text,
+    productCategory varchar(70),
+    productPrice decimal,
+	productQuantity int,
+);
+
+create table ShoppingCart(
+    customerId int,
+	productId int,
+	primary key(customerId, productId)
+
+);
+
+
+
+
+ALTER TABLE shoppingCart
+ADD CONSTRAINT shoppingCart_Customer_fk 
+FOREIGN KEY (customerId) REFERENCES customer(customerId);
+
+ALTER TABLE shoppingCart
+ADD CONSTRAINT shoppingCart_Product_fk 
+FOREIGN KEY (productId) REFERENCES products(productId);
