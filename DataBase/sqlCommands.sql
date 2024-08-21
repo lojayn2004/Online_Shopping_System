ff756dc810bf259bd192c56edc5ed7d1828b1b96
lojayn2004 <111882914+lojayn2004@users.noreply.github.com>
2024-08-21 22:45:17 +0300

    Update sqlCommands.sql

--- DataBase/sqlCommands.sql
+++ DataBase/sqlCommands.sql
@@ -1,4 +1,33 @@
+create database OnlineShoppingSystem;
 
+create table [User](
+    userId int identity(1, 1) primary key,
+	userName varchar(70) unique,
+	[password] varchar(70),
+	email varchar(50)
+
+);
+
+create table [Admin] (
+   adminId int primary key,
+   dateOfEmployment Date
+
+);
+
+create table Customer (
+    customerId int primary Key,
+    streetCode varchar(50),
+	zipCode varchar(50),
+	city varchar(50)
+);
+
+alter table [Admin]
+add constraint fk_admin foreign key(adminId) references [User](userId)
+on delete cascade;
+
+alter table [Customer]
+add constraint fk_customer foreign key(customerId) references [User](userId)
+on delete cascade;
 
 create table Products(
     productId int primary key identity(1, 1),
@@ -16,6 +45,14 @@ create table ShoppingCart(
 	primary key(customerId, productId)
 
 );
+
+ALTER TABLE shoppingCart
+ADD CONSTRAINT shoppingCart_Customer_fk 
+FOREIGN KEY (customerId) REFERENCES customer(customerId);
+
+ALTER TABLE shoppingCart
+ADD CONSTRAINT shoppingCart_Product_fk 
+FOREIGN KEY (productId) REFERENCES products(productId);
 
 INSERT INTO Products (productName, productDescription, productCategory, productPrice, productQuantity)
 VALUES ('Smartphone', 'Latest 5G smartphone with 128GB storage', 'Electronics', 699.99, 50);
@@ -44,14 +81,3 @@ VALUES ('T-shirt', 'Cotton T-shirt with a cool print', 'Clothes', 19.99, 150);
 INSERT INTO Products (productName, productDescription, productCategory, productPrice, productQuantity)
 VALUES ('Running Shoes', 'Comfortable running shoes with good grip', 'Shoes', 79.99, 100);
 
-
-
-
-
-ALTER TABLE shoppingCart
-ADD CONSTRAINT shoppingCart_Customer_fk 
-FOREIGN KEY (customerId) REFERENCES customer(customerId);
-
-ALTER TABLE shoppingCart
-ADD CONSTRAINT shoppingCart_Product_fk 
-FOREIGN KEY (productId) REFERENCES products(productId);
